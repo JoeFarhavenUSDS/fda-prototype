9867ab95fd8044fc79c13def5ac1db30e46545ca
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 19:01:12 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = ({ children, siteTitle }) => (
   <header className="usa-header usa-header--extended" role="banner">
     <div className="usa-navbar" style={{backgroundColor: '#222c67', padding: '10px', width: '100%'}}>
-      <img src="images/FDA_Logo_Icon_White.svg" style={{height:60, paddingTop: '10px', paddingBottom: '10px'}} />
+      <img src="images/FDA_Logo_Icon_White.svg" style={{height:80, paddingTop: '10px', paddingBottom: '10px'}} />
       <ul style={{listStyleType: 'none', float: 'right'}}>
         <li style={{display: 'inline-block'}}><a title="" id="btn-search" style={{borderColor: 'rgb(229,182,17)', borderRadius: '3px', borderStyle: 'solid', borderWidth: '1px', fontFamily: 'Helvetica, Arial', fontSize: '12px', fontWeight: '400', paddingBottom: '5px', paddingLeft: '10px', paddingTop: '5px', paddingRight: '10px', color: 'white'}}>
           <span class="fa fa-search" aria-hidden="true">&nbsp;</span>
