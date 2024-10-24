5501bed57575337005482556cff15611f81098a1
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 14:50:05 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,7 +45,47 @@ const Layout = ({ children }) => {
       </a>
       <Banner />
       <div className="usa-overlay" />
-      {children}
+<div class="row fda-masthead">
+							<div class="col-xs-4 col-md-8"> 
+					<a href="/" title="FDA Homepage">
+						<h1 class="fda-masthead__fda-logo">U.S. Food and Drug Administration</h1>
+					</a>
+       	</div>
+				
+        <div class="col-xs-8 col-md-4">
+          <ul class="fda-masthead__item-list">
+            <li><a title="" id="btn-search" class="btn btn-default btn-sm fda-masthead__btn-search">
+              <span class="fa fa-search" aria-hidden="true">&nbsp;</span>
+              <span class="fda-masthead__btn-label">Search</span>
+            </a></li>
+            <li><a id="menu-btn" class="btn btn-default btn-sm fda-masthead__btn-menu collapsed" href="#primary-nav" data-toggle="collapse" aria-expanded="true">
+              <span class="fa fa-bars" aria-hidden="true">&nbsp;</span>
+              <span class="fda-masthead__btn-label">Menu</span>
+            </a></li>
+          </ul>
+				</div>
+							
+						<form class="fda-masthead__search sr-only" role="search" action="/search" method="GET" name="searchForm" id="search-form" accept-charset="UTF-8">
+							
+							<div class="search-popover" id="search-popover">
+								<div class="input-group pull-right" id="search-group">
+									<label class="sr-only" for="search-query">Search FDA</label>
+									
+									<input class="form-control search-input" id="search-query" name="s" aria-autocomplete="list" aria-haspopup="true" title="Enter the terms you wish to search for." placeholder="Search FDA" type="text"> 
+									
+									<span class="input-group-btn" id="input-group-btn">
+										
+										<button type="submit" class="btn btn-danger search-btn" id="search-btn" title="Search"><span class="fa fa-search" aria-hidden="true"><span class="sr-only">Submit search</span></span></button>
+
+									</span>
+
+								</div>
+							</div>
+																				
+						</form>					
+
+			</div>
+  {children}
       <Footer />
     </>
   );
