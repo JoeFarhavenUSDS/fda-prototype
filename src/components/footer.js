bbe542e5ddd291b022708dfccbc94a9ced4cd777
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 19:39:08 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import logo from 'uswds/img/logo-img.png';
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
     <div className="footer-section-bottom" style={{backgroundColor: 'rgb(226, 226, 228)', textAlign: 'center', width: '100%'}}>
-      <ul style={{listStyleType: 'none', color: 'black'}}>
+      <ul style={{listStyleType: 'none', color: 'black !important'}}>
         <li style={{padding:'10px'}}><a href=''>FDA Archive</a></li>
         <li style={{padding:'10px'}}><a href=''>About FDA</a></li>
         <li style={{padding:'10px'}}><a href=''>Accessibility</a></li>
@@ -16,9 +16,9 @@ const Footer = () => (
         <li style={{padding:'10px'}}><a href=''>HHS.gov</a></li>
         <li style={{padding:'10px'}}><a href=''>USA.gov</a></li>
       </ul>
-    </div>
-    <div style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%'}}>
-      <a href="" class="btn btn-default btn-md">Contact FDA</a>
+      <div style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%'}}>
+        <a href="">Contact FDA</a>
+      </div>
     </div>
   </footer>
 );
