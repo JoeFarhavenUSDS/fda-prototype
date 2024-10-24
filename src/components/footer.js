8e64fea4e9908ffb41d6de96b46244b6a01e93c2
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 19:58:25 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,21 +4,18 @@ import logo from 'uswds/img/logo-img.png';
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
     <div className="footer-section-bottom" style={{backgroundColor: 'rgb(226, 226, 228)', textAlign: 'center', width: '100%'}}>
-      <ul style={{listStyleType: 'none', color: 'rgb(46,41,37)', textDecorationColor: 'rgb(46,41,37)'}}>
-        <li style={{padding:'10px'}}><a href=''>FDA Archive</a></li>
-        <li style={{padding:'10px'}}><a href=''>About FDA</a></li>
-        <li style={{padding:'10px'}}><a href=''>Accessibility</a></li>
-        <li style={{padding:'10px'}}><a href=''>Visitor Information</a></li>
-        <li style={{padding:'10px'}}><a href=''>Website Policies / Privacy</a></li>
-        <li style={{padding:'10px'}}><a href=''>No FEAR Act</a></li>
-        <li style={{padding:'10px'}}><a href=''>Vulnerability Disclosure Policy</a></li>
-        <li style={{padding:'10px'}}><a href=''>FOIA</a></li>
-        <li style={{padding:'10px'}}><a href=''>HHS.gov</a></li>
-        <li style={{padding:'10px'}}><a href=''>USA.gov</a></li>
+      <ul style={{listStyleType: 'none'}}>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>FDA Archive</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>About FDA</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>Accessibility</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>Visitor Information</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>Website Policies / Privacy</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>No FEAR Act</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>Vulnerability Disclosure Policy</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>FOIA</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>HHS.gov</a></li>
+        <li style={{padding:'10px'}}><a href='' style={{color: 'rgb(46,41,37)'}}>USA.gov</a></li>
       </ul>
-      <div style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%', color: 'white', textDecorationColor: 'white', borderColor: 'rgb(204, 204, 204)', borderRadius: '4px', borderWidth: '1px', borderStyle: 'solid', paddingBottom: '5px', paddingLeft: '10px', paddingRight: '10px', paddingTop: '5px', verticalAlign: 'middle', lineHeight: '20px'}}>
-        <a href="">Contact FDA</a>
-      </div>
     </div>
   </footer>
 );
