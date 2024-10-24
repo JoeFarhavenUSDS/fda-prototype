06e281d136cb51e809ea096bfcf94dc5aa0a8566
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 19:18:31 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,33 +3,22 @@ import logo from 'uswds/img/logo-img.png';
 
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
-    <div className="footer-section-bottom bg-base-lighter">
-      <div className="grid-container">
-        <div className="grid-row padding-3">
-          <div className="tablet:grid-col">
-            <div className="logo-links">
-              <a className="footer-logo media_link" href="https://18f.gsa.gov">
-                <img src={logo} width="50" alt="Agency logo" />
-              </a>
-            </div>
-          </div>
-          <div className="tablet:grid-col"></div>
-          <div className="tablet:grid-col">
-            <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
-            </ul>
-          </div>
-          <div className="tablet:grid-col">
-            <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div className="footer-section-bottom" style={{backgroundColor: 'rgb(226, 226, 228)', textAlign: 'center', width: '100%'}}>
+      <ul>
+        <li><a href=''>FDA Archive</a></li>
+        <li><a href=''>About FDA</a></li>
+        <li><a href=''>Accessibility</a></li>
+        <li><a href=''>Visitor Information</a></li>
+        <li><a href=''>Website Policies / Privacy</a></li>
+        <li><a href=''>No FEAR Act</a></li>
+        <li><a href=''>Vulnerability Disclosure Policy</a></li>
+        <li><a href=''>FOIA</a></li>
+        <li><a href=''>HHS.gov</a></li>
+        <li><a href=''>USA.gov</a></li>
+      </ul>
+    </div>
+    <div className="footer-section-bottom" style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%'}}>
+      <a href="" class="btn btn-default btn-md">Contact FDA</a>
     </div>
   </footer>
 );
