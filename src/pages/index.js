112a1f568da8229a8085a905a5338002c4ed7720
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 14:45:06 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Highlights from '../components/highlights';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <form class="usa-form usa-form--large">
+    <form class="usa-form usa-form--large padding-10">
       <fieldset class="usa-fieldset">
         <legend class="usa-legend usa-legend">Select one historical figure</legend>
         <div class="usa-radio">
