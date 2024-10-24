b2af06cf58a63793f5d3a8746021532ec6e1bcba
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 17:37:46 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,7 @@ const Header = ({ children, siteTitle }) => (
   <header className="usa-header usa-header--extended" role="banner">
     <div className="usa-navbar">
       <div className="usa-logo" id="extended-logo">
-        <em className="usa-logo__text">
-          <Link to="/" title="Home" aria-label="Home">
-            {siteTitle}
-          </Link>
-        </em>
+        <img src="images/FDA_Logo_Icon_White.svg" />
       </div>
       <button className="usa-menu-btn">Menu</button>
     </div>
