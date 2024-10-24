ba93611a3711fe0edb3364ce2cfea4adf1b4fbdf
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-24 19:31:52 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,20 +4,20 @@ import logo from 'uswds/img/logo-img.png';
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
     <div className="footer-section-bottom" style={{backgroundColor: 'rgb(226, 226, 228)', textAlign: 'center', width: '100%'}}>
-      <ul>
-        <li><a href=''>FDA Archive</a></li>
-        <li><a href=''>About FDA</a></li>
-        <li><a href=''>Accessibility</a></li>
-        <li><a href=''>Visitor Information</a></li>
-        <li><a href=''>Website Policies / Privacy</a></li>
-        <li><a href=''>No FEAR Act</a></li>
-        <li><a href=''>Vulnerability Disclosure Policy</a></li>
-        <li><a href=''>FOIA</a></li>
-        <li><a href=''>HHS.gov</a></li>
-        <li><a href=''>USA.gov</a></li>
+      <ul style={{listStyleType: 'none', color: 'black'}}>
+        <li style={{padding:'10px'}}><a href=''>FDA Archive</a></li>
+        <li style={{padding:'10px'}}><a href=''>About FDA</a></li>
+        <li style={{padding:'10px'}}><a href=''>Accessibility</a></li>
+        <li style={{padding:'10px'}}><a href=''>Visitor Information</a></li>
+        <li style={{padding:'10px'}}><a href=''>Website Policies / Privacy</a></li>
+        <li style={{padding:'10px'}}><a href=''>No FEAR Act</a></li>
+        <li style={{padding:'10px'}}><a href=''>Vulnerability Disclosure Policy</a></li>
+        <li style={{padding:'10px'}}><a href=''>FOIA</a></li>
+        <li style={{padding:'10px'}}><a href=''>HHS.gov</a></li>
+        <li style={{padding:'10px'}}><a href=''>USA.gov</a></li>
       </ul>
     </div>
-    <div className="footer-section-bottom" style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%'}}>
+    <div style={{backgroundColor: 'rgb(46, 41, 37)', textAlign: 'center', width: '100%'}}>
       <a href="" class="btn btn-default btn-md">Contact FDA</a>
     </div>
   </footer>
