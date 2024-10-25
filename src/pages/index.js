1e0f3342f3027e2774ea536ac332a4d0d1dce72f
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-25 16:41:22 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
 
   const handleChange = (event) => {
     setQuestionOne(event.target.value);
-    window.alert("Event: " + event.target.value) + " / Question: " + questionOne);
+    window.alert("Event: " + event.target.value + " / Question: " + questionOne);
   };
   
   return (
