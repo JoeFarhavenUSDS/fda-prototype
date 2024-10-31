61dca378648ac222e764c207816233e473fa96fe
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-31 16:43:57 -0400

    Update intake.js

--- src/pages/intake.js
+++ src/pages/intake.js
@@ -12,6 +12,7 @@ const IntakePage = () => {
   const [questionProductName, setQuestionProductName] = useState('');
   const [questionWhatHappened, setQuestionWhatHappened] = useState('');
   const [whatHappenedCharactersRemaining, setWhatHappenedCharactersRemaining] = useState(4000);
+  const [questionWhere, setQuestionWhere] = useState('');
   const [questionThree, setQuestionThree] = useState(new Map());
   const [questionWhenMonth, setQuestionWhenMonth] = useState('');
   const [questionWhenDay, setQuestionWhenDay] = useState('');
@@ -286,6 +287,81 @@ const IntakePage = () => {
                   </div>
                 </div>
               </fieldset>
+            </div>
+            <div class="usa-form-group">
+              <label class="usa-label" id="question-where-label" for="question-where"
+                >Where did you buy the product?<abbr title="required" class="usa-hint usa-hint--required">*</abbr></label
+              >
+              <div class="usa-hint" id="question-where-hint">Include as much detail as possible, including the brand</div>
+              <select 
+                    class="usa-select" 
+                    name="options" 
+                    id="options"
+                    style={{width: '100%'}} 
+                    id="question-where" 
+                    name="question-where"
+                    value={questionWhere}
+                    onChange={e => setQuestionWhere(e.target.value)}
+              >
+                <option value>- Select -</option>
+              	<option value="AL">Alabama</option>
+              	<option value="AK">Alaska</option>
+              	<option value="AZ">Arizona</option>
+              	<option value="AR">Arkansas</option>
+              	<option value="CA">California</option>
+              	<option value="CO">Colorado</option>
+              	<option value="CT">Connecticut</option>
+              	<option value="DE">Delaware</option>
+              	<option value="DC">District Of Columbia</option>
+              	<option value="FL">Florida</option>
+              	<option value="GA">Georgia</option>
+              	<option value="HI">Hawaii</option>
+              	<option value="ID">Idaho</option>
+              	<option value="IL">Illinois</option>
+              	<option value="IN">Indiana</option>
+              	<option value="IA">Iowa</option>
+              	<option value="KS">Kansas</option>
+              	<option value="KY">Kentucky</option>
+              	<option value="LA">Louisiana</option>
+              	<option value="ME">Maine</option>
+              	<option value="MD">Maryland</option>
+              	<option value="MA">Massachusetts</option>
+              	<option value="MI">Michigan</option>
+              	<option value="MN">Minnesota</option>
+              	<option value="MS">Mississippi</option>
+              	<option value="MO">Missouri</option>
+              	<option value="MT">Montana</option>
+              	<option value="NE">Nebraska</option>
+              	<option value="NV">Nevada</option>
+              	<option value="NH">New Hampshire</option>
+              	<option value="NJ">New Jersey</option>
+              	<option value="NM">New Mexico</option>
+              	<option value="NY">New York</option>
+              	<option value="NC">North Carolina</option>
+              	<option value="ND">North Dakota</option>
+              	<option value="OH">Ohio</option>
+              	<option value="OK">Oklahoma</option>
+              	<option value="OR">Oregon</option>
+              	<option value="PA">Pennsylvania</option>
+              	<option value="RI">Rhode Island</option>
+              	<option value="SC">South Carolina</option>
+              	<option value="SD">South Dakota</option>
+              	<option value="TN">Tennessee</option>
+              	<option value="TX">Texas</option>
+              	<option value="UT">Utah</option>
+              	<option value="VT">Vermont</option>
+              	<option value="VA">Virginia</option>
+              	<option value="WA">Washington</option>
+              	<option value="WV">West Virginia</option>
+              	<option value="WI">Wisconsin</option>
+              	<option value="WY">Wyoming</option>
+                <option value="AS">American Samoa</option>
+                <option value="GU">Guam</option>
+                <option value="MP">Northern Mariana Islands</option>
+                <option value="PR">Puerto Rico</option>
+                <option value="UM">United States Minor Outlying Islands</option>
+                <option value="VI">Virgin Islands</option>
+              </select>
             </div>
           </div>
         }
