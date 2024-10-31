d2f00e54b9e9b8bae05644426a4ba630345fa8c0
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-10-31 14:33:06 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => {
         <h1>Report a problem</h1>
         <p>We review every submission. We use this information to monitor the safety of FDA-regulated products. </p>
         <p>We will ask you for the product information, what happened, and any photos of the product.  </p>
-        <p>We are unable to answer questions about submissions. You can check <a href="">the current list of recalled products</a>. </p>
+        <p>We are unable to answer questions about submissions. You can check <a href="https://www.fda.gov/safety/recalls-market-withdrawals-safety-alerts">the current list of recalled products</a>. </p>
         <p>We will reach out if we have any questions.</p>
         <div style={{width: '100%', textAlign: 'center'}}>
           <a href="/site/joefarhavenusds/fda-prototype/intake"><button class="usa-button" type="button" style={{width: '100%', textAlign: 'center'}}>Get Started</button></a>
